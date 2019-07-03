7231131c63fb7a601771f403182b54437c626efd
Alexander Lyabah <a.lyabah@checkio.org>
2019-07-03 16:42:39 +0300

    fix output

--- animation/init.js
+++ animation/init.js
@@ -9,7 +9,7 @@ requirejs(['ext_editor_1', 'ext_editor_io', 'jquery_190', 'raphael_210'],
         io.parseInputArguments = function (checkioInput) {
             var lang = top.Ext.INTERPRETER_SLUG.split('-')[0];
             if (lang === 'js') {
-                var args = ext.JSON.encode(checkioInput);
+                var args = JSON.encode(checkioInput);
                 return args.slice(1, args.length - 1);
             } else {
                 var checkioInputStr = checkioInput[0];
